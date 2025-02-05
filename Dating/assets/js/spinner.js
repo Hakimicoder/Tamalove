94fe71f8a4ca34e4f397ee1f3e264fe1df92c04f
Kamga Jumael <138940046+Hakimicoder@users.noreply.github.com>
2025-02-06 00:30:11 +0100

    Delete Dating directory

--- Dating/assets/js/spinner.js
+++
@@ -1,9 +0,0 @@
-let spin = document.getElementById("spinner");
-// console.log(spin);
-function closeSpin() {
-    spin.style.display = "block";
-    setTimeout(() => {
-        spin.style.display = "none";
-    }, 2000);
-};
-closeSpin();
